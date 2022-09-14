99af540d062b21b3f18ca4b087e0902656a7a6e8
hovhannes <johnnysc091@gmail.com>
2022-09-14 21:39:27 +0900

    Initial commit

---
+++ app/src/main/java/com/github/johnnysc/numberstesttask/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.johnnysc.numberstesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
