7e228d7fb12980c9052bdfbd38a892d286ea8940
hovhannes <johnnysc091@gmail.com>
2022-10-01 12:46:25 +0900

    provide instances

--- app/src/main/java/com/github/johnnysc/numberstesttask/main/sl/Core.kt
+++ app/src/main/java/com/github/johnnysc/numberstesttask/main/sl/Core.kt
@@ -15,7 +15,7 @@ interface Core : CloudModule, CacheModule, ManageResources {
 
     class Base(
         context: Context,
-        private val isRelease: Boolean
+        private val provideInstances: ProvideInstances
     ) : Core {
 
         private val manageResources: ManageResources = ManageResources.Base(context)
@@ -24,17 +24,11 @@ interface Core : CloudModule, CacheModule, ManageResources {
             DispatchersList.Base()
         }
         private val cloudModule by lazy {
-            if (isRelease)
-                CloudModule.Release()
-            else
-                CloudModule.Debug()
+            provideInstances.provideCloudModule()
         }
 
         private val cacheModule by lazy {
-            if (isRelease)
-                CacheModule.Base(context)
-            else
-                CacheModule.Mock(context)
+            provideInstances.provideCacheModule()
         }
 
         override fun <T> service(clasz: Class<T>): T = cloudModule.service(clasz)
